0e10bcc836a76be26ca82c1a251f3f9c9ec9415c
aditya <magaraditya2911@gmail.com>
2025-06-23 21:04:28 +0530

    Remove secrets file from version control

--- blog_app/lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const projectUrl = 'https://hicgovwroobwpmjqkdjz.supabase.co';
-  static const anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpY2dvdndyb29id3BtanFrZGp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMDQyMjUsImV4cCI6MjA2NTU4MDIyNX0.L2iRtn9mG44LBEeXkrGL81slr4voERf-j56XS5bJy9A';
-}
